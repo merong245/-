6d861a52d236b8234ce7506b0cb3f4e368e6789c
merong245 <37497183+merong245@users.noreply.github.com>
2022-03-27 01:13:19 +0900

    Create 14501 퇴사.py

---
+++ 구현/14501 퇴사.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+li = []
+for _ in range(n):
+    t, p = map(int,input().split())
+    li.append([t,p])
+
+max_num = 0
+
+for i in range(n):
+    for j in range(n):
+        
+    li[li][0]
+    max_num += li[i][1]
+print(li)
