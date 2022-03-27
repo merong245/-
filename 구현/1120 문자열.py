6bff9a789660cc1b58476e2dfa477bdb0e1bbbe1
merong245 <37497183+merong245@users.noreply.github.com>
2022-03-28 05:27:05 +0900

    Create 1120 문자열.py

---
+++ 구현/1120 문자열.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n , m = map(str,input().split())
+
+n = list(n)
+m = list(m)
+
+def cal(n,m):
+    res = 0
+    for i in range(len(n)):
+        if n[i] == '':
+            continue
+        if n[i] != m[i]:
+            res += 1
+    return res
+    
+ans = sys.maxsize
+    
+for _ in range(len(m)-len(n)+1):
+    ans = min(ans,cal(n,m))
+    n = [''] + n
+    
+    
+print(ans)
+
