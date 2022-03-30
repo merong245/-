0a7be46f2df889c3b935d13f6dd37394a733e342
merong245 <37497183+merong245@users.noreply.github.com>
2022-03-30 23:36:26 +0900

    Create 1149 RGB거리.py

---
+++ DP/1149 RGB거리.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+inf = sys.maxsize
+
+n= int(input())
+
+color = [list(map(int,input().split()))
+         for _ in range(n)]
+
+# dp n번까지 고른 비용의 최솟값
+dp = [[inf for _ in range(3)]
+      for _ in range(n)]
+dp[0][0] = color[0][0]
+dp[0][1] = color[0][1]
+dp[0][2] = color[0][2]
+
+for i in range(1,n):
+    dp[i][0] = min(dp[i-1][1],dp[i-1][2]) + color[i][0]
+    dp[i][1] = min(dp[i-1][0],dp[i-1][2]) + color[i][1]
+    dp[i][2] = min(dp[i-1][0],dp[i-1][1]) + color[i][2]
+
+print(min(dp[-1][0],dp[-1][1],dp[-1][2]))
+
