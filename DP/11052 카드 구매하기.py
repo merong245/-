c98786e8d2cab86d3edb9d61277f51d921199760
merong245 <37497183+merong245@users.noreply.github.com>
2022-04-08 19:49:23 +0900

    Update 11052 카드 구매하기.py

--- DP/11052 카드 구매하기.py
+++ DP/11052 카드 구매하기.py
@@ -8,7 +8,7 @@ cards = [0] + list(map(int,input().split()))
 dp = [0]* (n+1)
 
 dp[1] = cards[1]
-for i in range(1,n+1):
-    dp[i] = max(dp[i-1]+cards[1],cards[i])
+for i in range(2,n+1):
+    for j in range(1,i+1):
+        dp[i] = max(dp[i-j]+cards[j],dp[i])
 print(dp[-1])
-print(dp)
