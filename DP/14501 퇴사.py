c4084fb0ca9e21bc1b44dffa569cc448384976ee
merong245 <37497183+merong245@users.noreply.github.com>
2022-04-10 09:49:56 +0900

    Create 14501 퇴사.py

---
+++ DP/14501 퇴사.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+t = []
+p = []
+dp = [0]*(n+1)
+for i in range(n):
+    a,b = map(int,input().split())
+    t.append(a)
+    p.append(b)
+
+for i in range(n-1,-1,-1):
+    if t[i] + i > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], dp[i+t[i]]+p[i])
+    
+print(dp[0])
+    
+    
