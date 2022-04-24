d6764609459510cde0a2be5add101089665e8fc3
merong245 <37497183+merong245@users.noreply.github.com>
2022-04-24 22:16:04 +0900

    Create 1629 곱셈.py

---
+++ 구현/1629 곱셈.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+a,b,c = map(int,input().split())
+
+def power(a,b,c):
+    if b==1:
+        return a%c
+    elif b==2:
+        return a*a%c
+
+    else:
+        if b%2:
+            return ((power(a,b//2,c)**2)*a)%c
+        else:
+            return ((power(a,b//2,c)**2))%c
+print(power(a,b,c))
+
